69490936fa30a8bbab54d4f54ac1b79dab0da6a1
max-konin <mk@kernel-corp.com>
2013-04-27 16:55:35 +0700

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kerweb::Application.config.secret_token = '3318d9339422c54bc8cc408560700ff06c1497deaf64de74d7d53496dce0cc3fc81eda7b0422074dd4530ca093003d87c341281c73ce67c45d0f69dab84b1bc9'
