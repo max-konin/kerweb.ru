cdda709d59bb370851321eb3cd89e8b68cb15982
signal2564 <signal2564@gmail.com>
2013-05-01 23:31:47 +0700

    Commit with home page, some bootstrap and new controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kerweb::Application.config.secret_token = '326827460418b6eaf24c496ec41443c9388691cf9768596e20f7ba877ede445d8b5acb7de9a75181d5fc0de6deb0360659b47b9f2e5ada91299041bf2eaa70bf'
